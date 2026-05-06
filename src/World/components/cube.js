fc52f405f13bd6a897bcbc5e22c064d20acdd943
xu xuejian <2498492824@qq.com>
2026-05-06 17:52:08 +0800

    启用基于物理的正确的光照

--- src/World/components/cube.js
+++ src/World/components/cube.js
@@ -1,14 +1,24 @@
-import { BoxGeometry, Mesh, MeshBasicMaterial } from "three";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  MeshStandardMaterial,
+} from "three";
 
 function createCube() {
   // create a geometry；单位是 米
   const geometry = new BoxGeometry(2, 2, 2);
 
   // create a default (white) Basic material
-  const material = new MeshBasicMaterial();
+  // const material = new MeshBasicMaterial();
+
+  const material = new MeshStandardMaterial({ color: "green" });
 
   // create a Mesh containing the geometry and material
   const cube = new Mesh(geometry, material);
+
+  // 旋转立方体，非直视
+  cube.rotation.set(-0.5, -0.1, 0.8);
 
   return cube;
 }
